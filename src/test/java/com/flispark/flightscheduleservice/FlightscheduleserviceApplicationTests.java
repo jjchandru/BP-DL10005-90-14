08bbca8e41af96d1e66bec3afa9621d8e2df23c1
Chandrasekaran Janardhanan <jjchandru@yahoo.com>
2024-08-01 12:47:33 +0530

    uploading spring boot initial project files

---
+++ src/test/java/com/flispark/flightscheduleservice/FlightscheduleserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flispark.flightscheduleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightscheduleserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
